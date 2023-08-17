15a15862e664287b177c80654bc9ec4c138a6b18
Piyush Jain <piyushjain5350@gmail.com>
2023-08-17 20:12:17 +0530

    2 soln

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,15 +3,11 @@ const Modal = ({show , onClose}) => {
     if(!show){
        return null;
     }
-    let modalStyle={
-        border:'1px solid black',
-        padding:'10px',
-        margin:'10px 0'
-    }
+    
     return(
-     <div className="model-overlay" style={modalStyle}>
-        <div  className="model-close" >
-            <button onClick={onClose}>Close</button>
+     <div className="model-overlay">
+        <div  className="model-close" onClick={onClose}>
+                Close
        
          <p >This is the content of the modal.</p>
        </div>
